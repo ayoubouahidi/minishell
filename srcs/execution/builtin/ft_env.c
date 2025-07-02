b0464b493e3b4b4144624e5a9e25591c765df2cd
Elmostafa Khartiti <elkharti@c4r8p8.1337.ma>
2025-07-02 11:11:46 +0100

    fix some norm 42

--- srcs/execution/builtin/ft_env.c
+++ srcs/execution/builtin/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: elkharti <elkharti@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 10:00:00 by elkharti          #+#    #+#             */
-/*   Updated: 2025/07/01 20:25:15 by elkharti         ###   ########.fr       */
+/*   Updated: 2025/07/02 11:10:35 by elkharti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,65 @@ int	ft_env(t_data *data, char **args)
 	}
 	data->exit_status = SUCCESS;
 	return (SUCCESS);
+}
+static void	bubble_sort_env(t_env **sorted, int count)
+{
+	int		i;
+	int		j;
+	t_env	*temp;
+
+	i = 0;
+	while (i < count - 1)
+	{
+		j = 0;
+		while (j < count - i - 1)
+		{
+			if (ft_strcmp(sorted[j]->key, sorted[j + 1]->key) > 0)
+			{
+				temp = sorted[j];
+				sorted[j] = sorted[j + 1];
+				sorted[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+static void	print_sorted_env(t_env **sorted, int count)
+{
+	int	i;
+
+	i = 0;
+	while (i < count)
+	{
+		ft_putstr_fd("declare -x ", STDOUT_FILENO);
+		ft_putstr_fd(sorted[i]->key, STDOUT_FILENO);
+		if (sorted[i]->value && ft_strlen(sorted[i]->value) > 0)
+		{
+			ft_putstr_fd("=\"", STDOUT_FILENO);
+			ft_putstr_fd(sorted[i]->value, STDOUT_FILENO);
+			ft_putchar_fd('\"', STDOUT_FILENO);
+		}
+		ft_putchar_fd('\n', STDOUT_FILENO);
+		i++;
+	}
+}
+
+void	sort_and_print_env(t_env *env)
+{
+	t_env	*current;
+	t_env	*sorted[1000];
+	int		count;
+
+	count = 0;
+	current = env;
+	while (current && count < 1000)
+	{
+		sorted[count++] = current;
+		current = current->next;
+	}
+	
+	bubble_sort_env(sorted, count);
+	print_sorted_env(sorted, count);
 }
